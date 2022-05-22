3f9ba9731567d5757b86ce57a5163b6a858f1877
Lukman08 <lh41280@gmail.com>
2022-05-22 12:26:19 +0700

    homepage

--- resources/views/components/dcore/sidebar.blade.php
+++ resources/views/components/dcore/sidebar.blade.php
@@ -1,10 +1,10 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">SAID</a>
+            <a href="/home">SAID</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">SA</a>
+            <a href="/home">SA</a>
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
